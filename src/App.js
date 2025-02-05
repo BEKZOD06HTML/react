f4262b13bc06ba3ff189609eddbfe8878ceefb5e
BEKZOD06HTML <bekpulatoff3106@gmail.com>
2025-02-05 17:48:31 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/header/header';
+import Mid from './components/mid/mid';
+import Section from './components/section/section';
+import Footer from './components/footer/footer';
+function App() {
+  return (
+    <>
+      <Header />
+      <Mid />
+      <Section />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
