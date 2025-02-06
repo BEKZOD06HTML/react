4e43d81ea766268b5a0ff9280b5e91b9fbab2b83
BEKZOD06HTML <bekpulatoff3106@gmail.com>
2025-02-07 02:15:32 +0500

    first time

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './components/header/header';
 import Mid from './components/mid/mid';
 import Section from './components/section/section';
 import Footer from './components/footer/footer';
+
+
 function App() {
   return (
     <>
